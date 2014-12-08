abdddeb685dde290e9ca6c99cfd637c54dd4d407
Gjermund Bjaanes <bjaanes@gmail.com>
2014-12-08 20:29:21 +0100

    Made all pull-to-refresh controls black. Fixes #22

--- Roommates/HouseholdMembersPFQueryTableViewController.m
+++ Roommates/HouseholdMembersPFQueryTableViewController.m
@@ -40,7 +40,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+        
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
